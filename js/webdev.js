0ca8a88efa3ea2292f70134b5e4da6c1fdec09cb
Michael Kelly <mkelly@mozilla.com>
2015-03-17 11:51:54 -0700

    Update 2015-03-17T18:51:54.554Z

--- js/webdev.js
+++ js/webdev.js
@@ -3,6 +3,8 @@
 ;(function ($) {
     "use strict";
 
+    var PAGE_SIZE = 10;
+
     // Add expanding button for keyword lists that are too long.
     $(".keywords").each(function (_, list) {
         var $list = $(list);
@@ -19,4 +21,19 @@
             $list.append($showAll);
         }
     });
+
+    // Paginate project list.
+    var $projects = $(".project");
+    $projects.slice(PAGE_SIZE).hide();
+
+    var $paginator = $("<div></div>");
+    $("#projects").append($paginator);
+    $paginator.pagination({
+        items: $projects.length,
+        itemsOnPage: PAGE_SIZE,
+        onPageClick: function (page) {
+            var sliceStart = (page - 1) * PAGE_SIZE;
+            $projects.hide();
+            $projects.slice(sliceStart, sliceStart + PAGE_SIZE).show();
+        } });
 })(jQuery);
