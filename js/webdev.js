d35e19600fdecb74a8b7957384ddec2ad1fa7f91
Michael Kelly <mkelly@mozilla.com>
2015-03-18 11:57:35 -0700

    Update 2015-03-18T18:57:35.306Z

--- js/webdev.js
+++ js/webdev.js
@@ -24,6 +24,7 @@
 
     // Paginate project list.
     var $projects = $(".project");
+    $projects.addClass("matched"); // So pagination works before first search.
     $projects.slice(PAGE_SIZE).hide();
 
     var $paginator = $("<div></div>");
